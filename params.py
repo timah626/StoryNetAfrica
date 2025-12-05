bc53756e10411c182836345cbda154fc9c67d0b7
timah626 <timahchelsie@gmail.com>
2025-12-05 12:53:59 +0100

    params.py

---
+++ params.py
@@ -0,0 +1,5 @@
+# params.py
+from_email = "azohchelsie@gmail.com"
+# Use Gmail App Password (16 characters, no spaces)
+app_password = "pfmimyusjlfrbhgo"   # ← your real one here
+#tisp ptuh ljqj pmqt 
